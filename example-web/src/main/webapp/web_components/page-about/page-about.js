9fb8affcfe1d0c57b520e56090469fe547eb1961
Semen Goriachkin <artful.tiger@gmail.com>
2015-11-02 21:57:51 +0300

    some changes

--- example-web/src/main/webapp/web_components/page-about/page-about.js
+++ example-web/src/main/webapp/web_components/page-about/page-about.js
@@ -2,5 +2,16 @@
  * 
  */
 Polymer({
-	is : 'page-about'
+	is : 'page-about',
+	  created: function() {
+		    console.log(this.localName + '#' + this.id + ' was created');
+		  },
+
+		  attached: function() {
+		    console.log(this.localName + '#' + this.id + ' was attached');
+		  },
+
+		  detached: function() {
+		    console.log(this.localName + '#' + this.id + ' was detached');
+		  }
 });
--- example-web/src/main/webapp/web_components/page-home/page-home.js
+++ example-web/src/main/webapp/web_components/page-home/page-home.js
@@ -2,5 +2,16 @@
  * 
  */
 Polymer({
-	is : 'page-home'
+	is : 'page-home',
+	  created: function() {
+		    console.log(this.localName + '#' + this.id + ' was created');
+		  },
+
+		  attached: function() {
+		    console.log(this.localName + '#' + this.id + ' was attached');
+		  },
+
+		  detached: function() {
+		    console.log(this.localName + '#' + this.id + ' was detached');
+		  }
 });
